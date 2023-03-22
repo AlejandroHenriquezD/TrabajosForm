94a031f9ff991dee0fc55495be5523d6106f25ae
Alejandro <alejandro02.hd@gmail.com>
2023-03-22 14:55:40 +0000

    testg

--- formrecibedatos.php
+++ formrecibedatos.php
@@ -5,7 +5,7 @@ $datos = [];
 //Tipos de trabajos
 function tipos_trabajos()
 {
-    $tiposTrabajos = json_decode(file_get_contents("http://localhost/trabajosform/tipos_trabajos.php"), true);
+    $tiposTrabajos = json_decode(file_get_contents("http://localhost/trabajosform/tipo_trabajos"), true);
     echo "<h1>Tipos Trabajos</h1>";
     for ($p = 0; $p < count($tiposTrabajos); $p++) {
         echo "<p><input type='checkbox' id={{$tiposTrabajos[$p]['id']}} name={{$tiposTrabajos[$p]['id']}} value={{$tiposTrabajos[$p]['nombre']}} onclick={{tipos_articulos()}}/>" . "<label for={{$tiposTrabajos[$p]['id']}}>" . $tiposTrabajos[$p]['nombre'] . "</label></p>";
@@ -15,7 +15,7 @@ function tipos_trabajos()
 //Tipos de articulos
 function tipos_articulos()
 {
-    $tiposArticulos = json_decode(file_get_contents("http://localhost/trabajosform/tipos_articulos.php"), true);
+    $tiposArticulos = json_decode(file_get_contents("http://localhost/trabajosform/tipo_articulos"), true);
     echo "<h1>Tipos Articulos</h1>";
     for ($p = 0; $p < count($tiposArticulos); $p++) {
         echo "<p><input type='checkbox' id={{$tiposArticulos[$p]['id']}} name={{$tiposArticulos[$p]['id']}} value={{$tiposArticulos[$p]['nombre']}}/>" . "<label for={{$tiposArticulos[$p]['id']}}>" . $tiposArticulos[$p]['nombre'] . "</label></p>";
@@ -23,3 +23,4 @@ function tipos_articulos()
 }
 
 tipos_trabajos();
+tipos_articulos();
