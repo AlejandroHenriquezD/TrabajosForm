46f9b642d6274aca8441568af5d89171a5e6133c
Alejandro <alejandro02.hd@gmail.com>
2023-03-23 11:45:25 +0000

    Imagenes

--- proccess-form.php
+++ proccess-form.php
@@ -1,9 +1,20 @@
 <?php
+var_dump($_FILES);
 
-$name = $_POST["name"];
-$priority = filter_input(INPUT_POST, "priority", FILTER_VALIDATE_INT);
-$type = filter_input(INPUT_POST, "type", FILTER_VALIDATE_INT);
-$terms = filter_input(INPUT_POST, "terms", FILTER_VALIDATE_BOOL);
+$pathinfo = pathinfo($_FILES["image"]["name"]);
+
+$base = $pathinfo["filename"];
+
+$base = preg_replace("/[^\w-]/", "_", $base);
+
+$filename = $base . "." . $pathinfo["extension"];
+
+$destination = __DIR__ . "/uploads/" . $filename;
+
+$img = __DIR__ . "/uploads/" . $filename;
+$img_vectorizada = "img_vectorizada";
+$id_cliente = 1;
+$obsoleto = 0;
 
 // if ( ! $terms){
 //     die("Terms must be accepted");
@@ -23,7 +34,7 @@ if (mysqli_connect_errno()) {
     die("Connection error: " . mysqli_connect_errno());
 }
 
-$sql = "INSERT INTO tipos_trabajos (nombre) VALUES (?)";
+$sql = "INSERT INTO logos (img, img_vectorizada, obsoleto, id_cliente) VALUES (?,?,?,?)";
 
 $stmt = mysqli_stmt_init($conn);
 
@@ -31,9 +42,100 @@ if (! mysqli_stmt_prepare($stmt, $sql)) {
     die(mysqli_errno($conn));
 }
 
-mysqli_stmt_bind_param($stmt, "s",
-                       $name);
+mysqli_stmt_bind_param($stmt, "ssbi",
+                       $img,
+                       $img_vectorizada,
+                       $obsoleto,
+                       $id_cliente);
 
 mysqli_stmt_execute($stmt);
 
-echo "Registro Guardado.";
+echo "Registro Guardado."; 
+
+
+
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    exit('POST request method required');
+}
+
+if (empty($_FILES)) {
+    exit('$_FILES is empty - is file_uploads set to "Off" in php.ini?');
+}
+
+if ($_FILES["image"]["error"] !== UPLOAD_ERR_OK) {
+
+    switch ($_FILES["image"]["error"]) {
+        case UPLOAD_ERR_PARTIAL:
+            exit('File only partially uploaded');
+            break;
+        case UPLOAD_ERR_NO_FILE:
+            exit('No file was uploaded');
+            break;
+        case UPLOAD_ERR_EXTENSION:
+            exit('File upload stopped by a PHP extension');
+            break;
+        case UPLOAD_ERR_FORM_SIZE:
+            exit('File exceeds MAX_FILE_SIZE in the HTML form');
+            break;
+        case UPLOAD_ERR_INI_SIZE:
+            exit('File exceeds upload_max_filesize in php.ini');
+            break;
+        case UPLOAD_ERR_NO_TMP_DIR:
+            exit('Temporary folder not found');
+            break;
+        case UPLOAD_ERR_CANT_WRITE:
+            exit('Failed to write file');
+            break;
+        default:
+            exit('Unknown upload error');
+            break;
+    }
+}
+
+// Reject uploaded file larger than 1MB
+if ($_FILES["image"]["size"] > 1048576) {
+    exit('File too large (max 1MB)');
+}
+
+// Use fileinfo to get the mime type
+$finfo = new finfo(FILEINFO_MIME_TYPE);
+$mime_type = $finfo->file($_FILES["image"]["tmp_name"]);
+
+$mime_types = ["image/gif", "image/png", "image/jpeg"];
+        
+if ( ! in_array($_FILES["image"]["type"], $mime_types)) {
+    exit("Invalid file type");
+}
+
+// Replace any characters not \w- in the original filename
+$pathinfo = pathinfo($_FILES["image"]["name"]);
+
+$base = $pathinfo["filename"];
+
+$base = preg_replace("/[^\w-]/", "_", $base);
+
+$filename = $base . "." . $pathinfo["extension"];
+
+$destination = __DIR__ . "/uploads/" . $filename;
+
+// Add a numeric suffix if the file already exists
+$i = 1;
+
+while (file_exists($destination)) {
+
+    $filename = $base . "($i)." . $pathinfo["extension"];
+    $destination = __DIR__ . "/uploads/" . $filename;
+
+    $i++;
+}
+
+if ( ! move_uploaded_file($_FILES["image"]["tmp_name"], $destination)) {
+
+    exit("Can't move uploaded file");
+
+}
+
+echo "File uploaded successfully.";
+
+print_r($_FILES);
+
