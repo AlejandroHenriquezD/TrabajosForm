f572f17273497fe6b4386f8918a676114e1e955f
davidcasimiro77 <casimiroherreradavid2@gmail.com>
2023-04-20 14:50:38 +0100

    Scrollbar espacio

--- frontend/index.php
+++ frontend/index.php
@@ -165,7 +165,7 @@ echo "<!DOCTYPE html>
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
   <title>Index</title>
   <link rel='shortcut icon' href='favicon.png'>
-  <link rel='stylesheet' href='styles2.css'>
+  <link rel='stylesheet' href='styles.css'>
 </head>
 <body onload='primeraFuncion();'>
 <script>
