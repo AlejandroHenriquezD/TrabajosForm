8621b3234518df4499effbb8c88db4ceaf9886e5
davidcasimiro77 <casimiroherreradavid2@gmail.com>
2023-04-20 08:55:37 +0100

    arreglar filtro menus vol2

--- frontend/index.php
+++ frontend/index.php
@@ -165,7 +165,7 @@ echo "<!DOCTYPE html>
   <meta name='viewport' content='width=device-width, initial-scale=1.0'>
   <title>Index</title>
   <link rel='shortcut icon' href='favicon.png'>
-  <link rel='stylesheet' href='styles.css'>
+  <link rel='stylesheet' href='styles2.css'>
 </head>
 <body onload='primeraFuncion();'>
 <script>
