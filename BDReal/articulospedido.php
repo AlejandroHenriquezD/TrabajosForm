300419b0b4185ac061c7a3a4f8aacc9926cf3043
Alejandro <alejandro02.hd@gmail.com>
2023-04-27 10:00:32 +0000

    sql adpatada

---
+++ BDReal/articulospedido.php
@@ -0,0 +1,55 @@
+<?php
+include_once "numTienda.php";
+
+$serverName = "192.168.0.23\SQLEXIT,1433";
+$connectionOptions = array(
+    "Database" => "ExitERP0415",
+    "Uid" => "programacion",
+    "PWD" => "CU_2023",
+    "CharacterSet" => "UTF-8",
+    "TrustServerCertificate" => true
+);
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+$sql = "SELECT DISTINCT
+            CodigoAlmacen,
+            CodigoArticulo,
+            SeriePedido,
+            NumeroPedido,
+            DescripcionArticulo
+
+
+        FROM PedidoVentaLineas
+        WHERE    EjercicioPedido = 2023
+        AND (CodigoArticulo NOT LIKE ('6000%') OR CodigoArticulo NOT LIKE ('6001%') OR CodigoArticulo NOT LIKE ('6002%') OR CodigoArticulo NOT LIKE ('6003%'))
+        AND TipoArticulo = 'M'
+        
+        AND CodigoAlmacen = '". $tienda."'
+        ";
+
+$getResults = sqlsrv_query($conn, $sql);
+
+echo "<table>
+            <thead>
+                <th>Codigo Almacen</th>
+                <th>Codigo Articulo</th>
+                <th>Serie Pedido</th>
+                <th>Numero Pedido</th>
+                <th>Descripcion Articulo</th>
+
+            </thead>
+            <tbody>";
+            while($top = sqlsrv_fetch_array($getResults)){
+                echo "<tr>
+                        <td>". $top["CodigoAlmacen"] ."</td>
+                        <td>". $top["CodigoArticulo"] ."</td>
+                        <td>". $top["SeriePedido"] ."</td>
+                        <td>". $top["NumeroPedido"] ."</td>
+                        <td>". $top["DescripcionArticulo"] ."</td>
+   
+                      </tr>";
+            }
+      echo "</tbody>
+          </table>";
+
+?>
