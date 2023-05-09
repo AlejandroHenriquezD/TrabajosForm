2d4e0a2cae614ed65ff4a82a7ed1436d20077dca
JaviARo <javieralemanrodriguez@alumno.ieselrincon.es>
2023-05-09 14:36:28 +0100

    Trabajos y pedidos segun sesion

--- CRUDS/pedidos/menuPedidos.php
+++ CRUDS/pedidos/menuPedidos.php
@@ -1,23 +1,46 @@
 <?php
-echo "
+
+if(isset($_SESSION['usuario'])) {
+  echo "
     <link rel='stylesheet' href='../menu.css'>
     <div id='menu-lateral'>
-        <div id='desplegable-lateral' onclick='desplegarMenu()'>
-            <div id='flecha-lateral'></div>
-        </div>
-        <div id='enlaces-menu'>
-            <a href='../../frontend/index.php'>Formulario</a>
-            <a href='../bocetos/bocetos.php'>Bocetos</a>
-            <a href='../clientes/clientes.php'>Clientes</a>
-            <a href='../logos/logos.php'>Logos</a>
-            <a href='../posicion/posiciones.php'>Posiciones</a>
-            <a href='../tipoArticulo/tiposarticulo.php'>Tipos de artículo</a>
-            <a href='../tipoTrabajo/tipostrabajo.php'>Tipos de trabajo</a>
-            <a class='seleccionado' href='../pedidos/pedidos.php'>Pedidos</a>
-            <a href='../trabajos/trabajos.php'>Trabajos</a>
-            <a class='sesion' href='../../login/cerrarSesion.php'>Cerrar sesión</a>
-        </div>
-    </div>";
+      <div id='desplegable-lateral' onclick='desplegarMenu()'>
+        <div id='flecha-lateral'></div>
+      </div>
+      <div id='enlaces-menu'>
+        <a href='../../frontend/index.php'>Formulario</a>
+        <a href='../bocetos/bocetos.php'>Bocetos</a>
+        <a href='../clientes/clientes.php'>Clientes</a>
+        <a href='../logos/logos.php'>Logos</a>
+        <a href='../posicion/posiciones.php'>Posiciones</a>
+        <a href='../tipoArticulo/tiposarticulo.php'>Tipos de artículo</a>
+        <a href='../tipoTrabajo/tipostrabajo.php'>Tipos de trabajo</a>
+        <a class='seleccionado' href='../pedidos/pedidos.php'>Pedidos</a>
+        <a href='../trabajos/trabajos.php'>Trabajos</a>
+        <a class='sesion' href='../../login/cerrarSesion.php'>Cerrar sesión</a>
+      </div>
+    </div>
+  ";
+} else {
+  echo "
+    <link rel='stylesheet' href='../menu.css'>
+    <div id='menu-lateral'>
+      <div id='desplegable-lateral' onclick='desplegarMenu()'>
+        <div id='flecha-lateral'></div>
+      </div>
+      <div id='enlaces-menu'>
+        <a href='../../frontend/index.php'>Formulario</a>
+        <a href='../bocetos/bocetos.php'>Bocetos</a>
+        <a href='../clientes/clientes.php'>Clientes</a>
+        <a href='../logos/logos.php'>Logos</a>
+        <a class='seleccionado' href='../pedidos/pedidos.php'>Pedidos</a>
+        <a href='../trabajos/trabajos.php'>Trabajos</a>
+        <a class='sesion' href='../../login/login.php'>Iniciar sesión</a>
+      </div>
+    </div>
+  ";
+}
+
 ?>
 <?php include "../background.php" ?>
 <script>
--- CRUDS/trabajos/trabajos.php
+++ CRUDS/trabajos/trabajos.php
@@ -1,45 +1,57 @@
-<?php include "../sesion.php" ?>
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trabajos</title>
-    <link rel="shortcut icon" href="../../frontend/favicon.png">
-    <link rel="stylesheet" href="../../cruds.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Trabajos</title>
+  <link rel="shortcut icon" href="../../frontend/favicon.png">
+  <link rel="stylesheet" href="../../cruds.css">
 </head>
 
 <body>
-    <?php
+  <?php
 
-        $trabajos = json_decode(file_get_contents("http://localhost/trabajosform/trabajos"), true);
-        echo "<h1>TRABAJOS</h1>";
-        echo "<table>
-           <tr>
-              <th>Tienda</th>
-              <th>Num Pedido Venta</th>
-              <th>Posición</th>
-              <th>Cod Artículo</th>
-              <th>Tipo de trabajo</th>
-              <th>Fecha de pedido</th>
-              <th>Logo</th>
-              <th>Boceto</th>
-              <th>Orden de Trabajo</th>
-           </tr>";
-        for ($p = 0; $p < count($trabajos); $p++) {
-            $posicion = json_decode(file_get_contents("http://localhost/trabajosform/posiciones/" . $trabajos[$p]['id_posicion']), true);
-            $tipo_trabajo = json_decode(file_get_contents("http://localhost/trabajosform/tipo_trabajos/" . $trabajos[$p]['id_tipo_trabajo']), true);
-            $logo = json_decode(file_get_contents("http://localhost/trabajosform/logos/" . $trabajos[$p]['id_logo']), true);
-            $boceto = json_decode(file_get_contents("http://localhost/trabajosform/bocetos/" . $trabajos[$p]['id_boceto']), true);
+  $trabajos = json_decode(file_get_contents("http://localhost/trabajosform/trabajos"), true);
+  
+  if(!isset($_SESSION['usuario'])) {
+    include "../../BDReal/numTienda.php";
+    $trabajosTemp = array();
+    foreach($trabajos as $trabajo){
+      if($trabajo['num_tienda'] == $tienda){
+        array_push($trabajosTemp, $trabajo);
+      }
+    }
+    $trabajos = $trabajosTemp;
+  }
+  
+  echo "<h1>TRABAJOS</h1>";
+  echo "<table>
+          <tr>
+            <th>Tienda</th>
+            <th>Num Pedido Venta</th>
+            <th>Posición</th>
+            <th>Cod Artículo</th>
+            <th>Tipo de trabajo</th>
+            <th>Fecha de pedido</th>
+            <th>Logo</th>
+            <th>Boceto</th>
+            <th>Orden de Trabajo</th>
+          </tr>";
+  for ($p = 0; $p < count($trabajos); $p++) {
+    $posicion = json_decode(file_get_contents("http://localhost/trabajosform/posiciones/" . $trabajos[$p]['id_posicion']), true);
+    $tipo_trabajo = json_decode(file_get_contents("http://localhost/trabajosform/tipo_trabajos/" . $trabajos[$p]['id_tipo_trabajo']), true);
+    $logo = json_decode(file_get_contents("http://localhost/trabajosform/logos/" . $trabajos[$p]['id_logo']), true);
+    $boceto = json_decode(file_get_contents("http://localhost/trabajosform/bocetos/" . $trabajos[$p]['id_boceto']), true);
 
-            
-            
-            echo
-            "<tr class='fila'>
-           <td>" . $trabajos[$p]['num_tienda']. "</td> 
-           <td>" . $trabajos[$p]['ejercicio_pedido'].'/'. $trabajos[$p]['serie_pedido'].'/'. $trabajos[$p]['numero_pedido']  . "</td> 
+
+
+    echo
+    "<tr class='fila'>
+           <td>" . $trabajos[$p]['num_tienda'] . "</td> 
+           <td>" . $trabajos[$p]['ejercicio_pedido'] . '/' . $trabajos[$p]['serie_pedido'] . '/' . $trabajos[$p]['numero_pedido']  . "</td> 
            <td>" . $posicion['descripcion'] . "</td> 
            <td>" . $trabajos[$p]['codigo_articulo'] . "</td>
            <td>" . $tipo_trabajo['nombre'] . "</td>
@@ -47,34 +59,34 @@
            <td><img src='../." . $logo['img'] . "' alt='hola' height=150px></td>
            <td>";
 
-            if ($trabajos[$p]['id_boceto'] != null){
-                echo "<form action='../.".$boceto['pdf']."'>
+    if ($trabajos[$p]['id_boceto'] != null) {
+      echo "<form action='../." . $boceto['pdf'] . "'>
                         <button>Ver Boceto </button>
                       </form>";
-            }else {
-                echo "No Existe Boceto";
-            }
-           echo "
+    } else {
+      echo "No Existe Boceto";
+    }
+    echo "
             </td>
             
            <td>";
 
-           if ($trabajos[$p]['pdf'] != null){
-            echo "<form action='../.".$trabajos[$p]['pdf']."'>
+    if ($trabajos[$p]['pdf'] != null) {
+      echo "<form action='../." . $trabajos[$p]['pdf'] . "'>
                     <button>Ver Orden Trabajo</button>
                   </form>";
-            }else {
-                echo "Falta Orden Trabajo";
-            }
-            echo "
+    } else {
+      echo "Falta Orden Trabajo";
+    }
+    echo "
             </td>
 
             </tr>";
-        }
-        echo "</table>";
-    
-    ?>
-    <?php include "./menuTrabajos.php" ?>
+  }
+  echo "</table>";
+
+  ?>
+  <?php include "./menuTrabajos.php" ?>
 
 </body>
 
